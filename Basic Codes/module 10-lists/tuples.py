c1971c3a9e2afcfe05357c2ff6e98b66acc43379
Robert Padar <robertpadar@users.noreply.github.com>
2020-05-17 19:24:50 +0100

    copied into Basic Codes

---
+++ Basic Codes/module 10-lists/tuples.py
@@ -0,0 +1,25 @@
+#convert the following tuple into useful string:
+
+my_tuple = ('P','y','t','h','o','n','P','r','o','g','r','a','m','m','i','n','g')
+first = my_tuple[0]
+second = my_tuple[1]
+third = my_tuple[2]
+fourth = my_tuple[3]
+fifth = my_tuple[4]
+sixth = my_tuple[5]
+space = ' '
+seventh = my_tuple[6]
+eight = my_tuple[7]
+ninth = my_tuple[8]
+tenth = my_tuple[9]
+eleventh = my_tuple[10]
+twelweth = my_tuple[11]
+thirteenth = my_tuple[12]
+fourteenth = my_tuple[13]
+fifteenth = my_tuple[14]
+sixteenth = my_tuple[15]
+seventeenth = my_tuple[16]
+string = (first+second+third+fourth+fifth+sixth+space+seventh\
++eight+ninth+tenth+eleventh+twelweth+thirteenth+fourteenth\
++fifteenth+sixteenth+seventeenth)
+print(string)
