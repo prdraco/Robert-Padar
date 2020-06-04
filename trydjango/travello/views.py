6373b80b9249de48c57f25bd3e570f6333c20f2d
Robert Padar <robertpadar@users.noreply.github.com>
2020-06-04 16:20:25 +0100

    updated

--- trydjango/travello/views.py
+++ trydjango/travello/views.py
@@ -7,5 +7,7 @@ def index(request):
 
     dest1 = Destination()
     dest1.name = 'Paris'
+    dest1.desc = 'The city that never sleeps'
+    dest1.price = 399
 
     return render(request, 'index.html', {'dest1': dest1})
