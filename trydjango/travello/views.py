ba89b413b78a15dd2586a15758984f1b73fd0a29
Robert Padar <robertpadar@users.noreply.github.com>
2020-06-04 15:28:54 +0100

    updated

--- trydjango/travello/views.py
+++ trydjango/travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = 'Paris'
+
+    return render(request, 'index.html', {'dest1': dest1})
