5bd86dfa91bbbc44169568ba78e4bcc2e6b2e5c2
Robert Padar <robertpadar@users.noreply.github.com>
2020-06-04 19:23:27 +0100

    updated

--- trydjango/static/helloworld/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Products(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    sumamry = models.TextField(default='this is cool!')
