fbe86a929538b642ba55e82638ecc1fd653b1480
Robert Padar <robertpadar@users.noreply.github.com>
2020-06-04 18:47:10 +0100

    updated

--- trydjango/travello/views.py
+++ trydjango/travello/views.py
@@ -10,18 +10,21 @@ def index(request):
     dest1.desc = 'Nulla pretium tincidunt felis, nec.'
     dest1.price = 399
     dest1.image = 'destination_4.jpg'
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Bali'
     dest2.desc = 'The city that never sleeps'
     dest2.price = 699
     dest2.image = 'destination_2.jpg'
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'San Francisco'
     dest3.desc = 'Nulla pretium tincidunt felis, nec.'
     dest3.price = 559
     dest3.image = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
