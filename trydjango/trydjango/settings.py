958dc63ef4b73e9ebf5c7d4227d87af2e16cb5bf
Robert Padar <robertpadar@users.noreply.github.com>
2020-06-05 19:29:05 +0100

    updated

--- trydjango/trydjango/settings.py
+++ trydjango/trydjango/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'documents',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +76,11 @@ WSGI_APPLICATION = 'trydjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prdraco',
+        'USER': 'postgres',
+        'PASSWORD': 'Jelszohelye',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'templates/static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
