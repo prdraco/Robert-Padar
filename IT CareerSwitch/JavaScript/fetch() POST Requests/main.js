e6dee9dc59570294330dcb33ac6a4797c4c03359
Robert Padar <robertpadar@users.noreply.github.com>
2020-10-11 11:52:12 +0100

    updated

---
+++ IT CareerSwitch/JavaScript/fetch() POST Requests/main.js
@@ -0,0 +1,36 @@
+// Information to reach API
+const apiKey = '3fef675811b7495a8343841fbab6ea92';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  fetch(url, {method: 'POST', headers: {'Content-type': 'application/json',
+  'apikey': apiKey}, body: data}).then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+      console.log(networkError.message);
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
